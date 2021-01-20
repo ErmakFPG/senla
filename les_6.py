d8fd2cb5265113bdf8c8b1120bee5091679fe9f1
eremin <dm.eremin271090@gmail.com>
2021-01-20 17:30:05 +0300

    added class Item in task 6

--- les_6.py
+++ les_6.py
@@ -1,4 +1,15 @@
-from operator import itemgetter
+from operator import attrgetter
+
+
+class Item:
+    def __init__(self, name, cost, weight):
+        self.name = name
+        self.cost = cost
+        self.weight = weight
+        self.value = cost / weight
+
+    def add_in_backpack(self, backpack):
+        return backpack.append(self.name)
 
 
 try:
@@ -14,15 +25,15 @@ try:
             item_name = input('Введите название предмета: ')
             item_cost = float(input('Введите стоимость предмета: '))
             item_weight = float(input('Введите массу предмета: '))
-            items.append([item_name, item_cost, item_weight, item_cost / item_weight])
+            items.append(Item(item_name, item_cost, item_weight))
 
-        sorted_items = sorted(items, key=itemgetter(3, 2), reverse=True)
+        sorted_items = sorted(items, key=attrgetter('value', 'weight'), reverse=True)
 
         value_items = []
         for item in sorted_items:
-            if item[2] <= weight_limit:
-                value_items.append(item[0])
-                weight_limit -= item[2]
+            if item.weight <= weight_limit:
+                item.add_in_backpack(value_items)
+                weight_limit -= item.weight
 
         print(f'Эффективное заполнение рюкзака: {value_items}')
 
