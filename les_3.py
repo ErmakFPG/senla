f58717d47e253e700da282244a485fa534c7797a
eremin <dm.eremin271090@gmail.com>
2021-01-20 15:41:25 +0300

    added split_master-function in task 3

--- les_3.py
+++ les_3.py
@@ -1,6 +1,20 @@
+def split_master(string, delimiter=' '):
+    string_len = len(string)
+    result = []
+    start = 0
+    for i in range(string_len):
+        if string[i] == delimiter:
+            result.append(string[start:i])
+            start = i + 1
+
+    result.append(string[start:string_len])
+
+    return result
+
+
 while True:
     my_string = input('Введите строку с пробелами: ')
-    word_list = my_string.split()
+    word_list = split_master(my_string)
     word_count = len(word_list)
     my_string_sorted = ' '.join(sorted(word_list))
 
